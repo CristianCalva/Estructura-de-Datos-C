447efdcb9d4f0abcb1eb49911c0b66f392665073
cristian paul calva abad <cp.calvaa@uea.edu.ec>
2024-12-07 22:31:10 -0500

    first commit

---
+++ EstructuraDatos2425/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("=============================");
+Console.WriteLine("Universidad Estatal Amazonica");
+Console.WriteLine("=============================");
+Console.WriteLine();
+
---
+++ EstructuraDatos2425/Rectangulo.cs
@@ -0,0 +1,32 @@
+
+/// La clase rectangulo encapsula los atributos y proporciona metodos para calcular su area 
+public class Rectangulo{
+    private double Base;
+    private double Altura;
+
+
+/// <summary>
+/// Contructr de la clase rectangulo
+/// </summary>
+/// <param name="_base">La medidad de la base del rectangulo</param>
+/// <param name="_altura">La medidad de la altura del rectangulo</param>
+    public Rectangulo(double _base, double _altura){
+        Base = _base;
+        Altura = _altura;
+    }
+
+
+/// <summary>
+/// Funcion para calcular el area de un rectangulo
+/// </summary>
+/// <returns> Retorna un valor double </returns>
+    public double Area(){
+        return Base * Altura;
+
+    }
+
+    }
+
+    
+
+    
