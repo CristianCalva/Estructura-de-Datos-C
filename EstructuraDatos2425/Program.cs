09d8bdf3a994828f0300cd798b31cefa67d40423
cristian paul calva abad <cp.calvaa@uea.edu.ec>
2025-01-18 14:46:13 -0500

    listas

--- EstructuraDatos2425/Program.cs
+++ EstructuraDatos2425/Program.cs
@@ -1,1 +1,15 @@
-﻿Semana6.run();
+﻿Console.WriteLine("++++++++++++++++++++++++++++++");
+Console.WriteLine("UNIVERSIDAD ESTATAL AMAZONICA");
+Console.WriteLine("++++++++++++++++++++++++++++++");
+Console.WriteLine("");
+Console.WriteLine("+++ TECNOLOGIAS DE LA INFORMACION +++");
+Console.WriteLine("");
+Console.WriteLine(">>> CRISTIAN PAUL CALVA ABAD <<<");
+Console.WriteLine("");
+
+//semana2.run();
+//semana3.run();
+//Semana4.run();
+//Semana5.run();
+Semana6.run();
+
